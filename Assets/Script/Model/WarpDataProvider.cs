0084f528b4a94ddb2f1e5e7770c3d99da95a82b9
tarahiro <tarahiro64@gmail.com>
2024-11-02 19:42:13 +0900

    町の追加

--- Assets/Script/Model/WarpDataProvider.cs
+++ Assets/Script/Model/WarpDataProvider.cs
@@ -3,13 +3,28 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using gaw241020.Presenter;
+using Zenject;
 
 namespace gaw241020.Model
 {
     public class WarpDataProvider : MonoBehaviour,IWarpDataProvider
     {
-        [SerializeField]
         List<WarpData> m_WarpDataList;
+
+        [Inject]
+        public void Construct()
+        {
+            m_WarpDataList = new List<WarpData>();
+            for(int i= 0; i < transform.childCount; i++)
+            {
+                if(transform.GetChild(i).GetComponent<WarpData>() != null)
+                {
+                    m_WarpDataList.Add(transform.GetChild(i).GetComponent<WarpData>());
+                }
+            }
+           
+        }
+
 
         public IWarpData GetWarpData(string warpId)
         {
